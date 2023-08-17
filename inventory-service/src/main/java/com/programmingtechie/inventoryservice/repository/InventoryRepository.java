a012d5d9c537644a13c213a3b8a8f4dbd5b90cdc
Rajendra <trajendra0242@gmail.com>
2023-08-17 14:28:53 +0530

    first commit

---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.programmingtechie.inventoryservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.programmingtechie.inventoryservice.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
